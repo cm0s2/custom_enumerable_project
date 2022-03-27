c637064a3cc629030ad78c9bf2e124fe1c5927f4
Carl Gudmundsson <komoss@gmail.com>
2022-03-27 22:14:05 +0200

    Complete project

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,95 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    counter = 0
+    until counter == size
+      yield self[counter]
+      counter += 1
+    end
+    self
+  end
+
+  def my_each_with_index
+    counter = 0
+    until counter == size
+      yield(self[counter], counter)
+      counter += 1
+    end
+    self
+  end
+
+  def my_select
+    counter = 0
+    selected_array = []
+    until counter == size
+      selected_array << self[counter] if yield(self[counter])
+      counter += 1
+    end
+    selected_array
+  end
+
+  def my_all?
+    counter = 0
+    until counter == size
+      return false if !yield(self[counter])
+
+      counter += 1
+    end
+    true
+  end
+
+  def my_any?
+    counter = 0
+    until counter == size
+      return true if yield(self[counter])
+
+      counter += 1
+    end
+    false
+  end
+
+  def my_none?
+    counter = 0
+    until counter == size
+      return false if yield(self[counter])
+
+      counter += 1
+    end
+    true
+  end
+
+  def my_count
+    return size if !block_given?
+
+    counter = 0
+    num = 0
+    until counter == size
+      num += 1 if yield(self[counter])
+
+      counter += 1
+    end
+    num
+  end
+
+  def my_map
+    counter = 0
+    mapped_array = []
+    until counter == size
+      mapped_array << yield(self[counter])
+      counter += 1
+    end
+    mapped_array
+  end
+
+  # |sum, value| sum + value
+  def my_inject(initial_value = 0)
+    counter = 0
+    sum = initial_value
+    until counter == size
+      sum = yield(sum, self[counter])
+      counter += 1
+    end
+    sum
+  end
 end
